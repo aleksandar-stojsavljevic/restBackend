4413b6016eab8e344de09d9548acdaa43595218f
AleksandarStojsavljevic <aleksandarstojsavljevic@gmail.com>
2021-01-12 21:29:18 +0100

    socket

--- app.js
+++ app.js
@@ -69,6 +69,15 @@ mongoose
     { useUnifiedTopology: true, useNewUrlParser: true }
   )
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+      },
+    });
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => console.log(err));
